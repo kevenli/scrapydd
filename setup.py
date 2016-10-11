526fb89505f7865ebdbd018b27ca9143d92f832d
kevenli <pbleester@gmail.com>
2016-10-11 10:34:25 +0800

    update setup.py metadata

--- setup.py
+++ setup.py
@@ -8,6 +8,10 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name         = 'scrapydd',
     version      = version,
+    author       ='kevenli',
+    author_email ='pbleester@gmail.com',
+    url          = 'http://github.com/kevenli/scrapydd',
+
     packages     = find_packages(),
     package_data = {
         'scrapydd': [
@@ -22,4 +26,11 @@ setup(
             'scrapydd = scrapydd.scripts.cmdline:main',
         ]
     },
+    install_requires=[
+        'tornado>=4.4.2',
+        'apscheduler',
+        'scrapyd',
+        'sqlalchemy',
+        'sqlalchemy-migrate',
+    ]
 )
