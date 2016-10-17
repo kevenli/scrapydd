dc963e510b304056814abbd8294d1e2189189751
kevenli <pbleester@gmail.com>
2016-10-17 14:43:29 +0800

    command line usage

--- scrapydd/scripts/cmdline.py
+++ scrapydd/scripts/cmdline.py
@@ -5,6 +5,11 @@ import scrapydd.scheduleutil
 
 def main():
     argv = sys.argv
+    if len(argv) == 1:
+        print 'No command specified.'
+        print_commands()
+        sys.exit(1)
+        return
     cmd = argv[1]
     if cmd == 'agent':
         scrapydd.executor.run(argv)
@@ -12,6 +17,17 @@ def main():
         scrapydd.main.run(argv)
     elif cmd == 'add_schedule':
         scrapydd.scheduleutil.add_schedule()
+    else:
+        print 'Invalid command.'
+        print_commands()
 
+def print_commands():
+    print 'usage: scrapydd {command} [options]'
+    print 'Available commands:'
+    print '\tserver:\trun scrapydd server.'
+    print '\tagent:\trun scrapydd agent.'
+    print '\tadd_schedule:\tadd a schedule to spider.'
+    print ''
+    print 'use scrapydd {command} --help for further help.'
 if __name__ == '__main__':
     main()
