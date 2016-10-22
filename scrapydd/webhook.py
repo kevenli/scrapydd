20f2ed34e5d6ffb7bad6f5aa76dedd56f5fe2217
kevenli <pbleester@gmail.com>
2016-10-22 17:31:29 +0800

    webhook use isolated files, move on complete

--- scrapydd/webhook.py
+++ scrapydd/webhook.py
@@ -5,6 +5,7 @@ from tornado.httpclient import AsyncHTTPClient, HTTPRequest
 import logging
 from tornado.concurrent import Future
 from models import Session, WebhookJob, SpiderWebhook
+import os, os.path, shutil
 
 logger = logging.getLogger(__name__)
 
@@ -73,6 +74,14 @@ class DatabaseWebhookJobStateStorage(WebhookJobStateStorage):
         session.commit()
         session.close()
 
+    def job_failed(self, job_id):
+        session = Session()
+        job = session.query(WebhookJob).filter_by(id=job_id).first()
+        job.status = 3
+        session.add(job)
+        session.commit()
+        session.close()
+
     def get_next_job(self):
         session = Session()
         next_jobs = list(session.query(WebhookJob).filter(WebhookJob.status == 0).order_by(WebhookJob.id).slice(0, 1))
@@ -106,13 +115,15 @@ class DatabaseWebhookJobStateStorage(WebhookJobStateStorage):
 class WebhookJobExecutor():
     def __init__(self, job):
         self.payload_url = job.payload_url
-        self.item_file = open(job.items_file, 'r')
+        self.item_file = None
         self.future = Future()
         self.job = job
         self.ioloop = IOLoop.current()
+        self.send_msg_interval = 1
 
     def start(self):
-        self.ioloop.call_later(1, self.send_next_msg)
+        self.item_file = open(self.job.items_file, 'r')
+        self.ioloop.call_later(self.send_msg_interval, self.send_next_msg)
         return self.future
 
     def send_next_msg(self, callback_future=None):
@@ -134,17 +145,18 @@ class WebhookJobExecutor():
         else:
             self.finish_job()
 
-
     def finish_job(self):
         if self.item_file:
             self.item_file.close()
         self.future.set_result(self.job)
 
     def schedule_next_send(self, future=None):
-        self.ioloop.call_later(1, self.send_next_msg)
+        self.ioloop.call_later(self.send_msg_interval, self.send_next_msg)
 
 
 class WebhookDaemon():
+    queue_file_dir = 'webhook'
+
     def __init__(self):
         logger.debug('webhookdaemon start')
         self.ioloop = IOLoop.current()
@@ -153,15 +165,22 @@ class WebhookDaemon():
         self.storage = DatabaseWebhookJobStateStorage()
         self.poll_next_job_callback.start()
 
+        if not os.path.exists(self.queue_file_dir):
+            os.makedirs(self.queue_file_dir)
+
     def check_job(self):
         logger.debug('webhook daemon checkjob.')
         if self.current_job is None:
             next_job = self.storage.get_next_job()
             if next_job:
                 self.current_job = WebhookJobExecutor(next_job)
-                future = self.current_job.start()
-                self.storage.start_job(next_job.id)
-                future.add_done_callback(self.job_finised)
+                try:
+                    future = self.current_job.start()
+                    self.storage.start_job(next_job.id)
+                    future.add_done_callback(self.job_finised)
+                except Exception as e:
+                    logger.error('Error when starting webhook job: %s', e.message)
+                    self.job_failed(next_job)
 
 
     def init(self):
@@ -171,11 +190,22 @@ class WebhookDaemon():
         job = future.result()
         self.storage.finish_job(job.id)
         self.current_job = None
+        if os.path.exists(job.items_file) and os.path.dirname(job.items_file) == self.queue_file_dir:
+            os.remove(job.items_file)
         logger.info('webhook job %s finished', job.id)
+
+    def job_failed(self, job):
+        self.storage.job_failed(job.id)
+        self.current_job = None
+        if os.path.exists(job.items_file) and os.path.dirname(job.items_file) == self.queue_file_dir:
+            os.remove(job.items_file)
+        logger.info('webhook job %s failed', job.id)
 
     def on_spider_complete(self, job, items_file):
         session = Session()
         webhook = session.query(SpiderWebhook).filter_by(id = job.spider_id).first()
+        task_items_file = os.path.join(self.queue_file_dir, os.path.basename(items_file))
+        shutil.copy(items_file, task_items_file)
         if webhook:
-            self.storage.add_job(job.id, webhook.payload_url, items_file)
+            self.storage.add_job(job.id, webhook.payload_url, task_items_file)
         session.close()
