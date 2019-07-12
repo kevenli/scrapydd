13beacb8ad9d1c7e4f52009a3143152a9ff88f05
kevenli <pbleester@gmail.com>
2019-07-12 13:41:56 +0800

    test_project v1.2.0

--- tests/test_project/setup.py
+++ tests/test_project/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'test_project',
-    version      = '1.0',
+    version      = '1.2',
     packages     = find_packages(),
     entry_points = {'scrapy': ['settings = test_project.settings']},
     install_requires = ['scrapy'],
