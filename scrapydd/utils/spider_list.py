3e6db378d1019d58cb5ed3ec1e1d5e9e8187727b
kevenli <pbleester@gmail.com>
2019-09-17 15:01:51 +0800

    py3 compatible

--- scrapydd/utils/spider_list.py
+++ scrapydd/utils/spider_list.py
@@ -3,6 +3,8 @@ import pkg_resources
 from scrapy.spiderloader import SpiderLoader
 from scrapy.settings import Settings
 import sys
+from six import next
+
 
 def activate_egg(eggpath):
     """Activate a Scrapy egg file. This is meant to be used from egg runners
@@ -10,13 +12,14 @@ def activate_egg(eggpath):
     leave unwanted side effects.
     """
     try:
-        d = pkg_resources.find_distributions(eggpath).next()
+        d = next(pkg_resources.find_distributions(eggpath))
     except StopIteration:
         raise ValueError("Unknown or corrupt egg")
     d.activate()
     settings_module = d.get_entry_info('scrapy', 'settings').module_name
     os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_module)
     return settings_module
+
 
 def main(egg_path=None):
     settings_module = activate_egg(egg_path)
@@ -26,6 +29,7 @@ def main(egg_path=None):
     loader = SpiderLoader(settings)
     for spider in loader.list():
         print(spider)
+
 
 if __name__ == '__main__':
     main(sys.argv[1])
