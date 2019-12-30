86c8f79d9733f6b6a89a335a45b2b5b13bf6ca97
Keven Li <pbleester@gmail.com>
2019-12-30 16:21:21 +0800

    add rest tests

---
+++ tests/handlers/test_rest.py
@@ -0,0 +1,130 @@
+from ..base import AppTest, ProjectWorkspaceStub
+from os import path
+from scrapydd.poster.encode import multipart_encode, ensure_binary
+from scrapydd.models import Project, session_scope
+from scrapydd.main import make_app
+from scrapydd.schedule import SchedulerManager
+from scrapydd.nodes import NodeManager
+from scrapydd.webhook import WebhookDaemon
+from scrapydd.settting import SpiderSettingLoader
+from tornado.concurrent import Future
+from scrapydd.config import Config
+from scrapydd.exceptions import InvalidProjectEgg, ProcessFailed
+from six.moves.urllib.parse import urlencode
+
+
+base_dir = path.join(path.dirname(path.dirname(__file__)))
+
+
+class AddVersionHandlerTest(AppTest):
+    def test_post(self):
+        project_name = 'test_project'
+        post_data = {}
+        post_data['egg'] = open(path.join(base_dir, 'test_project-1.0-py2.7.egg'), 'rb')
+        post_data['project'] = project_name
+        post_data['version'] = '1.0'
+        post_data['_xsrf'] = 'dummy'
+
+        datagen, headers = multipart_encode(post_data)
+        databuffer = b''.join([ensure_binary(x) for x in datagen])
+        headers['Cookie'] = "_xsrf=dummy"
+        response = self.fetch('/addversion.json', method='POST', headers=headers, body=databuffer)
+
+        self.assertEqual(200, response.code)
+
+        with session_scope() as session:
+            project = session.query(Project).filter_by(name=project_name).first()
+            self.assertIsNotNone(project)
+            self.assertEqual(project.name, project_name)
+
+    def test_post_create(self):
+        project_name = 'test_project'
+        postdata = {'project': project_name}
+        response = self.fetch('/delproject.json', method='POST', body=urlencode(postdata))
+        self.assertIn(response.code, [404, 200])
+        post_data = {}
+        post_data['egg'] = open(path.join(base_dir, 'test_project-1.0-py2.7.egg'), 'rb')
+        post_data['project'] = project_name
+        post_data['version'] = '1.0'
+        post_data['_xsrf'] = 'dummy'
+
+        datagen, headers = multipart_encode(post_data)
+        databuffer = b''.join([ensure_binary(x) for x in datagen])
+        headers['Cookie'] = "_xsrf=dummy"
+        response = self.fetch('/addversion.json', method='POST', headers=headers, body=databuffer)
+
+        self.assertEqual(200, response.code)
+
+        with session_scope() as session:
+            project = session.query(Project).filter_by(name=project_name).first()
+            self.assertIsNotNone(project)
+            self.assertEqual(project.name, project_name)
+
+class AddVersionHandlerTest_InvalidProjectEgg(AppTest):
+    class InvalidProjectWorkspaceStub(ProjectWorkspaceStub):
+        def spider_list(self):
+            future = Future()
+            future.set_exception(InvalidProjectEgg())
+            return future
+
+    def test_post(self):
+        project_name = 'test_project'
+        post_data = {}
+        post_data['egg'] = open(path.join(base_dir, 'test_project-1.0-py2.7.egg'), 'rb')
+        post_data['project'] = project_name
+        post_data['version'] = '1.0'
+        post_data['_xsrf'] = 'dummy'
+
+        datagen, headers = multipart_encode(post_data)
+        databuffer = b''.join([ensure_binary(x) for x in datagen])
+        headers['Cookie'] = "_xsrf=dummy"
+        response = self.fetch('/addversion.json', method='POST', headers=headers, body=databuffer)
+
+        self.assertEqual(400, response.code)
+
+    def get_app(self):
+        config = Config()
+        scheduler_manager = SchedulerManager(config=config)
+        scheduler_manager.init()
+        node_manager = NodeManager(scheduler_manager)
+        node_manager.init()
+        webhook_daemon = WebhookDaemon(config, SpiderSettingLoader())
+        webhook_daemon.init()
+        return make_app(scheduler_manager, node_manager, webhook_daemon, secret_key='123',
+                        project_workspace_cls=AddVersionHandlerTest_InvalidProjectEgg.InvalidProjectWorkspaceStub,
+                        project_storage_dir='./test_data')
+
+
+class AddVersionHandlerTest_ProcessFail(AppTest):
+    class ProcessFailProjectWorkspaceStub(ProjectWorkspaceStub):
+        def spider_list(self):
+            future = Future()
+            future.set_exception(ProcessFailed())
+            return future
+
+    def test_post(self):
+        project_name = 'test_project'
+        post_data = {}
+        post_data['egg'] = open(path.join(base_dir, 'test_project-1.0-py2.7.egg'), 'rb')
+        post_data['project'] = project_name
+        post_data['version'] = '1.0'
+        post_data['_xsrf'] = 'dummy'
+
+        datagen, headers = multipart_encode(post_data)
+        databuffer = b''.join([ensure_binary(x) for x in datagen])
+        headers['Cookie'] = "_xsrf=dummy"
+        response = self.fetch('/addversion.json', method='POST', headers=headers, body=databuffer)
+
+        self.assertEqual(400, response.code)
+
+    def get_app(self):
+        config = Config()
+        scheduler_manager = SchedulerManager(config=config)
+        scheduler_manager.init()
+        node_manager = NodeManager(scheduler_manager)
+        node_manager.init()
+        webhook_daemon = WebhookDaemon(config, SpiderSettingLoader())
+        webhook_daemon.init()
+        return make_app(scheduler_manager, node_manager, webhook_daemon, secret_key='123',
+                        project_workspace_cls=AddVersionHandlerTest_ProcessFail.ProcessFailProjectWorkspaceStub,
+                        project_storage_dir='./test_data')
