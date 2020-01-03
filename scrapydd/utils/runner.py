1d6629d7df67141c6c20894b2ec823a2559124ec
Keven Li <pbleester@gmail.com>
2020-01-03 17:14:07 +0800

    runner install requirements

--- scrapydd/utils/runner.py
+++ scrapydd/utils/runner.py
@@ -7,6 +7,7 @@ from ..eggstorage import FilesystemEggStorage
 import os
 import pkg_resources
 from six import next
+from subprocess import Popen, PIPE
 
 
 def activate_egg(eggpath):
@@ -21,6 +22,7 @@ def activate_egg(eggpath):
     d.activate()
     settings_module = d.get_entry_info('scrapy', 'settings').module_name
     os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_module)
+    return d
 
 
 @contextmanager
@@ -43,6 +45,13 @@ def project_environment(project):
         if eggpath:
             os.remove(eggpath)
 
+def install_requirements(distribute):
+    requires = [x.key for x in distribute.requires()]
+    if requires:
+        p = Popen([sys.executable, '-m', 'pip', 'install', ','.join(requires)], stdout=PIPE, stderr=PIPE)
+        print(p.stdout.read())
+        return p.wait()
+    return 0
 
 def main():
     from scrapy.cmdline import execute
@@ -50,7 +59,10 @@ def main():
     settings = Settings()
     egg_path = os.environ.get('SCRAPY_EGG')
     if egg_path:
-        activate_egg(egg_path)
+        distribute = activate_egg(egg_path)
+        ret = install_requirements(distribute)
+        if ret > 0:
+            sys.exit(ret)
         settings_module = os.environ.get('SCRAPY_SETTINGS_MODULE')
         if settings_module:
             settings.setmodule(settings_module, priority='project')
