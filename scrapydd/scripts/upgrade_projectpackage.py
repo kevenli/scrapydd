562e899cf2890300084e22cd9376cbd18c6f0fc4
KevenLi <pbleester@gmail.com>
2020-01-18 23:16:30 +0800

    skip project if upgrade failed

--- scrapydd/scripts/upgrade_projectpackage.py
+++ scrapydd/scripts/upgrade_projectpackage.py
@@ -1,8 +1,12 @@
+import logging
+from tornado.ioloop import IOLoop
 from scrapydd.models import Project, ProjectPackage, session_scope
 from scrapydd.storage import ProjectStorage
 from scrapydd.config import Config
-from scrapydd.workspace import RunnerFactory
-from tornado.ioloop import IOLoop
+from scrapydd.workspace import RunnerFactory, ProcessFailed, InvalidProjectEgg
+
+
+logger = logging.getLogger(__name__)
 
 
 def upgrade():
@@ -16,12 +20,15 @@ def upgrade():
             storage = ProjectStorage(config.get('project_storage_dir'), project)
             version, eggf = storage.get_egg()
             runner = runner_factory.build(eggf)
-            project_settings_module = ioloop.run_sync(runner.settings_module)
-            spider_list = ioloop.run_sync(runner.list)
-            package = ProjectPackage()
-            package.project = project
-            package.type = 'scrapy'
-            package.settings_module = project_settings_module
-            package.spider_list = ','.join(spider_list)
-            session.add(package)
-            session.commit()
+            try:
+                project_settings_module = ioloop.run_sync(runner.settings_module)
+                spider_list = ioloop.run_sync(runner.list)
+                package = ProjectPackage()
+                package.project = project
+                package.type = 'scrapy'
+                package.settings_module = project_settings_module
+                package.spider_list = ','.join(spider_list)
+                session.add(package)
+                session.commit()
+            except (ProcessFailed, InvalidProjectEgg) as ex:
+                logger.error(ex)
