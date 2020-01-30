c4577309336728c97ce1c23ccaac2006bbb1eab2
KevenLi <pbleester@gmail.com>
2020-01-31 00:06:52 +0800

    refactor test_executor

--- tests/test_executor.py
+++ tests/test_executor.py
@@ -1,44 +1,49 @@
-import unittest
-import logging
+"""
+Tests for executor module
+"""
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 import os.path
+import logging
 from tornado.concurrent import Future
-from tornado.ioloop import IOLoop
 from tornado.testing import AsyncHTTPTestCase, gen_test
 from tornado.web import RequestHandler, Application
 from tornado.httpclient import HTTPError
-from scrapydd.agent import AgentConfig
-from scrapydd.executor import SpiderTask, TaskExecutor, ProjectEggDownloader
+from scrapydd.executor import ProjectEggDownloader
+
+LOGGER = logging.getLogger(__name__)
 
 
-@unittest.skip
-class TaskExecutorTests(unittest.TestCase):
-    def test_execute(self):
-        task =SpiderTask()
-        task.spider_name
-        config = AgentConfig()
+class HandlerBase(RequestHandler):
+    def data_received(self, chunk):
+        pass
 
 
-class FakeEggDownloader(object):
+# pylint: disable=too-few-public-methods
+class FakeEggDownloader:
     def __init__(self, egg_filepath):
         self.egg_filepath = egg_filepath
 
     def download_egg_future(self, spider_id):
+        LOGGER.info('download_egg_future, spider_id: %s', spider_id)
         ret_future = Future()
         ret_future.set_result(self.egg_filepath)
         return ret_future
 
 
-class DownloadEggHandler(RequestHandler):
+# pylint: disable=arguments-differ
+class DownloadEggHandler(HandlerBase):
     def get(self, job_id):
         if job_id == '404':
             return self.set_status(404, 'Not Found')
 
-        with open('tests/test_project-1.0-py2.7.egg', 'rb') as f:
-            self.write(f.read())
+        with open('tests/test_project-1.0-py2.7.egg', 'rb') as f_egg:
+            return self.write(f_egg.read())
 
 
-class DownloadEgg404Handler(RequestHandler):
+class DownloadEgg404Handler(HandlerBase):
     def get(self, job_id):
+        LOGGER.info('DownloadEgg404Handler.get, job_id: %s', job_id)
         self.set_status(404, 'Not Found')
 
 
@@ -67,26 +72,7 @@ class ProjectEggDownloaderTest(AsyncHTTPTestCase):
         job_id = '404'
         target = ProjectEggDownloader(self.get_url('/some_error_request/'))
         try:
-            download_path = yield target.download_egg_future(job_id)
+            yield target.download_egg_future(job_id)
             self.fail("No exception caught")
-        except HTTPError as e:
-            self.assertEqual(404, e.code)
-
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    task = SpiderTask()
-    task.spider_id = 1
-    task.project_name = 'test_project'
-    task.spider_name = 'success_spider'
-    task.project_version = '1.0'
-    task.id = 1
-    task.spider_parameters = {}
-
-    def execute(future=None):
-        executor = TaskExecutor(task, FakeEggDownloader('test_project/dist/test_project-1.0-py2.7.egg'), '/work')
-        executor.execute().add_done_callback(execute)
-    #period = PeriodicCallback(execute, 1000)
-    #period.start()
-    execute()
-    IOLoop.current().start()
+        except HTTPError as ex:
+            self.assertEqual(404, ex.code)
