6cb3bc7c1b9efee4d48811a3f2b4b133895885db
KevenLi <pbleester@gmail.com>
2020-02-03 19:48:46 +0800

    plugin.py add generate(), perform() methods

--- scrapydd/utils/plugin.py
+++ scrapydd/utils/plugin.py
@@ -3,6 +3,7 @@ import json
 import sys
 import subprocess
 import os
+import argparse
 
 
 def install_requirements(distribute, append_log=False):
@@ -30,7 +31,7 @@ def install_requirements(distribute, append_log=False):
 
 def execute(plugin_name):
     try:
-        entry_point = list(pkg_resources.iter_entry_points('scrapydd.plugin',
+        entry_point = list(pkg_resources.iter_entry_points('scrapydd.spliderplugin',
                                                            plugin_name))[0]
     except StopIteration:
         sys.stderr.write('Cannot find plugin %s' % plugin_name)
@@ -68,12 +69,82 @@ def desc(egg_path):
     print(output)
 
 
+def _pop_command_name(argv):
+    return argv.pop(1)
+
+
+def generate(plugin_names=None, output_file=None):
+    output_dict = {}
+    for entry_point in pkg_resources.iter_entry_points('scrapydd.spliderplugin'):
+        plugin_name = entry_point.name
+        if plugin_names and plugin_name not in plugin_names:
+            continue
+        output_dict[entry_point.name] = {}
+        plugin_cls = entry_point.load()
+        plugin = plugin_cls()
+        plugin_desc = json.loads(plugin.desc())
+
+        for parameter_key, parameter in plugin_desc['parameters'].items():
+            parameter_value = parameter.get('default_value')
+            if parameter_value is None:
+                parameter_value = ''
+            output_dict[entry_point.name][parameter_key] = parameter_value
+
+    output = json.dumps(output_dict, indent=4, sort_keys=True)
+    if output_file:
+        with open(output_file, 'w') as f:
+            f.write(output)
+    else:
+        print(output)
+
+
+def perform(base_module=None, output_file=None):
+    with open('test.json', 'r') as f:
+        settings = json.load(f)
+
+    if output_file:
+        output_stream = open(output_file, 'w')
+    else:
+        output_stream = sys.stdout
+
+    if base_module:
+        output_stream.write('from %s import *' % base_module)
+    for entry_point in pkg_resources.iter_entry_points(
+            'scrapydd.spliderplugin'):
+        plugin_name = entry_point.name
+        plugin_cls = entry_point.load()
+        plugin = plugin_cls()
+        output_stream.write(plugin.execute(settings[plugin_name]))
+
+
 def main():
-    cmd = sys.argv[1]
+    argv = sys.argv
+    cmd = _pop_command_name(argv)
     if cmd == 'execute':
         return execute(sys.argv[2])
     elif cmd == 'desc':
         return desc(sys.argv[2])
+    elif cmd == 'generate':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--output', dest='output', required=False,
+                            default=None, help='output json file')
+        parser.add_argument('plugin_name', nargs='*',
+                            help='select plugin to populate settings, '
+                            'if no plugin specified, populate all '
+                            'plugins installed.')
+        args = parser.parse_args()
+        return generate(output_file=args.output, plugin_names=args.plugin_name)
+    elif cmd == 'perform':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-b', '--base', dest='base', required=False,
+                            default=None, help='base module to inherit.')
+        parser.add_argument('--output', dest='output', required=False,
+                            default=None, help='output module file')
+        args = parser.parse_args()
+        return perform(base_module=args.base, output_file=args.output)
+    else:
+        raise Exception('Not supported cmd %s' % cmd)
+
 
 if __name__ == '__main__':
     main()
