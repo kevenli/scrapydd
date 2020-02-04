238cddb58d975733fedef0f04b8ef86275b680fd
KevenLi <pbleester@gmail.com>
2020-02-04 23:25:31 +0800

    utils.runner remove EggStorage

--- scrapydd/utils/runner.py
+++ scrapydd/utils/runner.py
@@ -1,15 +1,8 @@
 import sys
-import shutil
-import tempfile
 import os
 import pkg_resources
-from contextlib import contextmanager
-if os.name == 'posix' and sys.version_info[0] < 3:
-    import subprocess32 as subprocess
-else:
-    import subprocess
-from six import next
-from ..eggstorage import FilesystemEggStorage
+import subprocess
+
 
 def activate_egg(eggpath):
     """Activate a Scrapy egg file. This is meant to be used from egg runners
@@ -24,27 +17,6 @@ def activate_egg(eggpath):
     settings_module = d.get_entry_info('scrapy', 'settings').module_name
     os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_module)
     return d
-
-
-@contextmanager
-def project_environment(project):
-    eggstorage = FilesystemEggStorage({})
-    version, eggfile = eggstorage.get(project)
-    if eggfile:
-        prefix = '%s-%s-' % (project, version)
-        fd, eggpath = tempfile.mkstemp(prefix=prefix, suffix='.egg')
-        lf = os.fdopen(fd, 'wb')
-        shutil.copyfileobj(eggfile, lf)
-        lf.close()
-        activate_egg(eggpath)
-    else:
-        eggpath = None
-    try:
-        assert 'scrapy.conf' not in sys.modules, "Scrapy settings already loaded"
-        yield
-    finally:
-        if eggpath:
-            os.remove(eggpath)
 
 
 def install_requirements(distribute, append_log=False):
@@ -70,27 +42,24 @@ def install_requirements(distribute, append_log=False):
     return 0
 
 
-def main(argv=None):
+def main():
     from scrapy.cmdline import execute
     from scrapy.settings import Settings
-    settings = Settings()
+    settings = None
     egg_path = os.environ.get('SCRAPY_EGG')
+
+    settings_module = os.environ.get('SCRAPY_SETTINGS_MODULE')
+    if settings_module:
+        settings = Settings()
+        settings.setmodule(settings_module, priority='project')
+
     if egg_path:
         distribute = activate_egg(egg_path)
         ret = install_requirements(distribute)
         if ret > 0:
             sys.exit(ret)
-        settings_module = os.environ.get('SCRAPY_SETTINGS_MODULE')
-        if settings_module:
-            settings.setmodule(settings_module, priority='project')
-        extra_settings_module = os.environ.get('SCRAPY_EXTRA_SETTINGS_MODULE')
-        if extra_settings_module:
-            settings.setmodule(extra_settings_module)
-        return execute(argv=argv, settings=settings)
-    project = os.environ['SCRAPY_PROJECT']
-    with project_environment(project):
-        execute()
 
+    execute(settings=settings)
 
 if __name__ == '__main__':
     main()
