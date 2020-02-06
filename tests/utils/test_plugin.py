61276f57878d6c9b59bb3d1491d4c603c2025ab7
Keven Li <pbleester@gmail.com>
2020-02-06 14:08:03 +0800

    build test_project when testing

--- tests/utils/test_plugin.py
+++ tests/utils/test_plugin.py
@@ -1,6 +1,17 @@
 import unittest
 import os
+import shutil
+from setuptools import Distribution, find_packages
 from scrapydd.utils.plugin import desc
+
+from contextlib import contextmanager
+
+@contextmanager
+def cd(path):
+    old_dir = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_dir)
 
 TEST_PACKAGE_PATH = os.path.join(os.path.dirname(__file__),
                                  '..',
@@ -11,4 +22,37 @@ TEST_PACKAGE_PATH = os.path.join(os.path.dirname(__file__),
 
 class PluginTest(unittest.TestCase):
     def test_desc(self):
-        desc(TEST_PACKAGE_PATH)
+        egg_path = TEST_PLUGIN_EGG_PATH
+        desc(egg_path)
+
+OUTPUT_DIR = 'output'
+def build_sample_plugin():
+    plugin_src_dir = os.path.join(os.path.dirname(__file__),
+                                  '../../samples/scrapy-splitvariants-plugin')
+    with cd(plugin_src_dir):
+        shutil.rmtree('dist')
+        d = Distribution(dict(
+            version='1.0',
+            name='scrapy_splitvariants_plugin',
+            description='scrapy_splitvariants_plugin',
+            packages=find_packages(exclude=['tests', 'tests.*']),
+            entry_points={
+                'scrapydd.spliderplugin': [
+                    'splitvariants = scrapy_splitvariants_plugin.plugin:Plugin',
+                ],
+            },
+            install_requires=[
+                'scrapy',
+                'scrapy-splitvariants'
+            ],
+        ))
+        d.script_name = 'setup.py'
+        d.script_args = ['--quiet', 'clean', 'bdist_egg']
+        d.parse_command_line()
+        d.run_commands()
+
+        egg_name = os.listdir('dist')[0]
+        return os.path.abspath(os.path.join('dist', egg_name))
+
+
+TEST_PLUGIN_EGG_PATH = build_sample_plugin()
