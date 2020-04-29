1e11ab1f0b054a3aec317fb583e8492691285e18
KevenLi <pbleester@gmail.com>
2020-04-29 14:10:51 +0800

    Fix absolute path not work as FeedURI on windows.

--- scrapydd/workspace.py
+++ scrapydd/workspace.py
@@ -220,11 +220,14 @@ class ProjectWorkspace(object):
                     '-s',
                     '%s=%s' % (spider_parameter_key, spider_parameter_value)
                 ]
-        pargs += ['-o', items_file]
+        pargs += ['-o', str(path_to_file_uri(items_file))]
 
         env = os.environ.copy()
         env['SCRAPY_PROJECT'] = str(self.project_name)
+        env['SCRAPY_FEED_URI'] = str(path_to_file_uri(items_file))
+        env['SCRAPY_FEED_FORMAT'] = 'jl'
         env['SCRAPY_EGG'] = 'spider.egg'
+
 
         p = Popen(pargs, env=env, stdout=f_output,
                   cwd=self.project_workspace_dir,
