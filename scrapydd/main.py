9629e9bae44ce0c4506922e56233cd2d6e11e8bd
KevenLi <pbleester@gmail.com>
2020-06-11 17:17:37 +0800

    admin users page

--- scrapydd/main.py
+++ scrapydd/main.py
@@ -555,6 +555,7 @@ def make_app(scheduler_manager, node_manager, webhook_daemon=None,
         (r'/admin$', admin.AdminHomeHandler),
         (r'^/admin/nodes$', admin.AdminNodesHandler),
         (r'^/admin/spiderplugins$', admin.AdminPluginsHandler),
+        (r'^/admin/users$', admin.AdminUsersHandler),
 
         # rest apis
         (r'^/api/projects/(\w+)/spiders/(\w+)/jobs/(\w+)', rest.GetProjectJob),
