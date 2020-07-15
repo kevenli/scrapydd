ddb325a43bead1df400eb6c9696ab3d9684f71a5
KevenLi <pbleester@gmail.com>
2020-07-15 14:17:09 +0800

    rollback runner

--- scrapydd/utils/runner.py
+++ scrapydd/utils/runner.py
@@ -4,7 +4,6 @@ import pkg_resources
 import subprocess
 import logging
 from scrapy.utils.project import get_project_settings
-from scrapy.settings import Settings
 
 LOGGER = logging.getLogger(__name__)
 
@@ -24,30 +23,6 @@ def activate_egg(eggpath):
     return d
 
 
-def activate_project(egg_path: str = None) -> Settings:
-    """
-    Activate a scrapy project environment.
-    This will load settings from current environment ( specified project egg or
-    inside a project folder), instantiate the Settings, install required package
-    of the project.
-    :param egg_path: A project package egg.
-    :return: Settings : Settings
-    """
-    if egg_path:
-        LOGGER.debug('activating egg %s', egg_path)
-        distribute = activate_egg(egg_path)
-        ret = install_requirements(distribute)
-        if ret > 0:
-            sys.exit(ret)
-        settings_module = os.environ.get('SCRAPY_SETTINGS_MODULE')
-        if settings_module:
-            settings = Settings()
-            settings.setmodule(settings_module, priority='project')
-    else:
-        settings = get_project_settings()
-    return settings
-
-
 def install_requirements(distribute, append_log=False):
     requires = [str(x) for x in distribute.requires()]
     if requires:
@@ -64,31 +39,34 @@ def install_requirements(distribute, append_log=False):
         p = subprocess.Popen(pargs, stdout=stdout, stderr=subprocess.PIPE,
                              env=env)
         try:
-            stdout, stderr = p.communicate(timeout=600)
-            # ret = p.returncode
-            return 0
+            ret = p.wait(timeout=60)
+            return ret
         except subprocess.TimeoutExpired:
             sys.stderr.write('pip install process timeout:\n')
             return 1
     return 0
 
 
-def execute(argv=None, settings=None):
-    from scrapy.cmdline import execute as scrapy_execute
-    scrapy_execute(argv=argv, settings=settings)
-
-
 def main(argv=None):
+    from scrapy.cmdline import execute
+    from scrapy.settings import Settings
+    settings = None
     egg_path = os.environ.pop('SCRAPY_EGG', None)
 
-    settings = activate_project(egg_path=egg_path)
+    if egg_path:
+        distribute = activate_egg(egg_path)
+        ret = install_requirements(distribute)
+        if ret > 0:
+            sys.exit(ret)
+        settings_module = os.environ.get('SCRAPY_SETTINGS_MODULE')
+        if settings_module:
+            settings = Settings()
+            settings.setmodule(settings_module, priority='project')
+    else:
+        settings = get_project_settings()
 
-    # TODO: replace the text-file-based SCRAPY_EXTRA_SETTINGS_MODULE with the
-    #  mechanism consist of `activate_project` and `execute` methods, to
-    #  provide a more robust and direct api to execute scrapy process.
-    #  `activate_project` method activate the project and return a Settings
-    #  instance , `execute` accept a Settings instance as the parameter.
-    #  Caller can manipulate Settings arbitrarily before `execute`.
+
+
     extra_settings_module = os.environ.pop('SCRAPY_EXTRA_SETTINGS_MODULE',
                                            None)
     if extra_settings_module:
@@ -97,7 +75,6 @@ def main(argv=None):
         settings.setmodule(extra_settings_module, priority='project')
 
     execute(argv=argv, settings=settings)
-
 
 if __name__ == '__main__':
     main()
