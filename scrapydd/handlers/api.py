11efdceee6c99e75cf41d8e2082cd44eedbc4921
KevenLi <pbleester@gmail.com>
2020-08-07 11:01:02 +0800

    update code description

--- scrapydd/handlers/api.py
+++ scrapydd/handlers/api.py
@@ -3,6 +3,17 @@ from tornado.web import Application
 
 
 class NodesHandler(RestBaseHandler):
+    """
+    Register a node.
+    A user (admin) can request a token from the server. (key, secret_key pair)
+    By this token, a node can be registered to the server.
+
+    The node is communicating with server by http/https at the
+    same port of ui now. There will be a grpc server in the future.
+    A node register to server by communicate to the api port, the api
+    return necessary info for the node to run. after the registration
+    it communicate to the server only on grpc endpoint.
+    """
     def post(self):
         pass
 
