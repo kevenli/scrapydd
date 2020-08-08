ecc1bc161ff9611c27c8242489977657af85507f
KevenLi <pbleester@gmail.com>
2020-08-08 12:53:04 +0800

    fix test

--- scrapydd/migrates/versions/028_node_id.py
+++ scrapydd/migrates/versions/028_node_id.py
@@ -7,7 +7,7 @@ def upgrade(migrate_engine):
     meta.bind = migrate_engine
     node_table = Table('nodes', meta, autoload=True)
     node_table_id = node_table.c.id
-    node_table_id.alter(type=BigInteger)
+    node_table_id.alter(type=BigInteger, autoincrement=True)
 
 
 def downgrade(migrate_engine):
