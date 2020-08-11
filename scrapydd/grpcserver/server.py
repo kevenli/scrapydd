a893968e3ea55c93ef8147374db7084050640daf
KevenLi <pbleester@gmail.com>
2020-08-11 16:09:25 +0800

    grpc heartbeat

--- scrapydd/grpcserver/server.py
+++ scrapydd/grpcserver/server.py
@@ -6,18 +6,49 @@ import sys
 from . import service_pb2
 from . import service_pb2_grpc
 from .grpc_asyncio import AsyncioExecutor
+from ..nodes import NodeManager
 
 
 logger = logging.getLogger(__name__)
 
 
+class SignatureValidationInterceptor(grpc.ServerInterceptor):
+    def __init__(self):
+        def abort(ignored_request, context):
+            context.abort(grpc.StatusCode.UNAUTHENTICATED, 'Invalid signature')
+
+        self._abortion = grpc.unary_unary_rpc_method_handler(abort)
+
+    def intercept_service(self, continuation, handler_call_details):
+        ticket = None
+        for k, v in handler_call_details.invocation_metadata:
+            if k == 'x-node-id':
+                ticket = v
+                break
+        if ticket:
+            handler_call_details.invocation_metadata.node_id = ticket
+            return continuation(handler_call_details)
+        else:
+            return self._abortion
+
+
 class NodeServicer(service_pb2_grpc.NodeServiceServicer):
+    def __init__(self, node_manager: NodeManager):
+        self._node_manager = node_manager
+
+    def get_node_id(self, context):
+        for key, value in context.invocation_metadata():
+            if key == 'x-node-id':
+                return value
+
     async def Heartbeat(self, request, context):
-        logger.debug('heartbeat')
+        node_id = self.get_node_id(context)
+        logger.debug('heartbeat, node: %s', node_id)
+        self._node_manager.heartbeat(node_id)
         return service_pb2.HeartbeatResponse()
 
 
-def start():
+def start(node_manager):
     if sys.platform == 'win32':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     port = '1337'
@@ -32,7 +63,7 @@ def start():
     server = grpc.server(AsyncioExecutor(loop=asyncio.new_event_loop()))
     #server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     #server = grpc.server(AsyncioExecutor())
-    service_pb2_grpc.add_NodeServiceServicer_to_server(NodeServicer(), server)
+    service_pb2_grpc.add_NodeServiceServicer_to_server(NodeServicer(node_manager), server)
 
     address = '[::]:' + port
     logger.info('starting grpc server on %s', address)
